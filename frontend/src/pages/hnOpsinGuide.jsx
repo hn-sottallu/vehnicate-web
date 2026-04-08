e04b2fa37d2e772226f13dd63ad7994d4a931fc8
Sai Siddharth Sottallu <ss.sottallu@gmail.com>
2026-04-09 00:04:49 +0530

    corrected sm fonts error

--- frontend/src/pages/hnOpsinGuide.jsx
+++ frontend/src/pages/hnOpsinGuide.jsx
@@ -1,8 +1,6 @@
 import { useState, useRef } from "react";
 
 const styles = `
-  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&family=DM+Mono:wght@300;400;500&family=Crimson+Pro:ital,wght@0,300;0,400;1,300;1,400&display=swap');
-
   :root {
     --black: #000;
     --white: #fff;
